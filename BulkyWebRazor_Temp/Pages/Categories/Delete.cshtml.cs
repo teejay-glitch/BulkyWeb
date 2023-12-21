4d3a760ee161fd4bb06612ed471eef1f3fead0a1
Tishan Jayalath <TishanJ@ipd.com.au>
2023-12-21 18:34:21 +0530

    completed razor crud operations

---
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        public readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(Category);
+            TempData["success"] = "Category deleted successfully";
+            _db.SaveChanges();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
