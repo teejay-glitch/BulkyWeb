6b3ee4c99648733ca26a76690a9b33a80e7cb2fc
Tishan Jayalath <Tishan Jayalath@DESKTOP-N98MNDR>
2024-01-21 11:33:50 +0530

    Place order implementation

--- BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -9,10 +9,12 @@ using System.Security.Claims;
 namespace BulkyWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
-    [Authorize(Roles = SD.Role_Customer)]
+    [Authorize(Roles = SD.Role_Customer + "," + SD.Role_Company)]
     public class ShoppingCartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty] // this is used in SummaryPOST. When the submit button is clicked,
+                       // following ShoppingCarVM will get populated.
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
         public ShoppingCartController(IUnitOfWork unitOfWork)
@@ -66,6 +68,75 @@ namespace BulkyWeb.Areas.Customer.Controllers
             }
 
             return View(ShoppingCartVM);
+        }
+
+        [HttpPost]
+        [ActionName("Summary")]
+        public IActionResult SummaryPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCartRepository.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product");
+
+            ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+            ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+
+            // since this is populated here, EF thinks that this also needs to be added to DB as a new entity, which is a new record.
+            // but here this record is already in the database and intrduces an error because the record has a key which is used in the table already
+            // therefore be aware of using navigation properties
+            // ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUserRepository.GetFirstOrDefault(u => u.Id == userId);
+
+            //here is the work around for the above issue.
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.GetFirstOrDefault(u => u.Id == userId);
+
+            foreach (ShoppingCart shoppingCart in ShoppingCartVM.ShoppingCartList)
+            {
+                shoppingCart.Price = GetPriceBasedOnQuantity(shoppingCart);
+                ShoppingCartVM.OrderHeader.OrderTotal += (shoppingCart.Price * shoppingCart.Count);
+            }
+
+            if(applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                //.GetValueOrDefault() -> CompanyId can be null. To match it with 0, we use this.
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+            } else
+            {
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+            }
+
+            _unitOfWork.OrderHeaderRepository.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+
+                _unitOfWork.OrderDetailRepository.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                //.GetValueOrDefault() -> CompanyId can be null. To match it with 0, we use this.
+                
+            }
+
+            return RedirectToAction(nameof(OrderConfirmation), new {id = ShoppingCartVM.OrderHeader.Id}); 
+            // this redirect to the order confirmation page with the model id
+        }
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            return View(id);
         }
 
         public IActionResult Plus(int cartId)
