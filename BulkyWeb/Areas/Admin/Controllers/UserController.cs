9ae837dd05cf938db513c112afebf7e56aa064d8
Tishan Jayalath <Tishan Jayalath@DESKTOP-N98MNDR>
2024-03-26 13:04:18 +0530

    Added role management option

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,129 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleManagement(string userId)
+        {
+            string RoleId = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem { Text = i.Name, Value = i.Id.ToString() }),
+            };
+
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+
+            return View(RoleVM);
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+
+            foreach (var user in userList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+
+            return Json(new { data = userList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+
+            if (user == null) return Json(new { success = false, message = "User does not exist" });
+
+            if (user.LockoutEnd != null && user.LockoutEnd > DateTime.Now)
+            {
+                user.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                user.LockoutEnd = DateTime.Now.AddDays(10);
+            }
+
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Locked state changed" });
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            string RoleId = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+
+            if (roleManagementVM.ApplicationUser.Role == oldRole)
+            {
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+
+                if (roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();W
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,68 @@
+﻿
+var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblUserData').DataTable({
+        "ajax": { url: '/admin/user/getAll' },
+        "columns": [
+            { data: "name", "width": "20%" },
+            { data: "email", "width": "15%" },
+            { data: "phoneNumber", "width": "10%" },
+            { data: "company.name", "width": "10%" },
+            { data: "role", "width": "10%" },
+            {
+                data: { id: "id", lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+
+                    if (lockout > today) {
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white">
+                                    <i class="bi bi-pencil-square"></i> Lock
+                                </a>
+                                <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white">
+                                    <i class="bi bi-pencil-square"></i> Permission
+                                </a>
+                            </div>
+                        `
+                    } else {
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white">
+                                    <i class="bi bi-pencil-square"></i> Unlock
+                                </a>
+                                <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white">
+                                    <i class="bi bi-pencil-square"></i> Permission
+                                </a>
+                            </div>
+                        `
+                    }
+
+
+                }
+            }
+
+        ]
+    });
+}
+
+function LockUnlock(id) {
+    $.ajax({
+        type: "POST",
+        url: "/admin/user/lockunlock",
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+        }
+    })
+}
+
